63cc50b58fec175699648f3a73902427b7eaa422
Kaushalprajapti <kaushalprajapatite@gmail.com>
2025-11-16 11:09:44 +0530

    fix/icon and base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: "/student_survay_PWA/",   // REQUIRED for GitHub Pages
+  // base: "/student_survay_PWA/",   // REQUIRED for GitHub Pages
   plugins: [
     react(),
     VitePWA({
